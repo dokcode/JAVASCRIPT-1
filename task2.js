c424db4c420a9616d060f940f4072c00aac2bd0c
Muhamad Iqbal Aprido <Edo.vivoy21@gmail.com>
2022-12-29 19:06:23 +0700

    Update task2.js

--- task2.js
+++ task2.js
@@ -8,31 +8,37 @@
 //    60 - 69 = D
 //    0 - 59 = E
 
-const mtk = 80;
+const mtk = 60;
 const BahasaIndonesia = 90;
 const BahasaInggris = 89;
 const ipa = 69;
 
 let total = mtk + BahasaIndonesia + BahasaInggris + ipa;
 let hitung = total / 4;
-switch (true) {
-  case hitung >= 0 && hitung <= 59:
-    console.log(`Rata-rata = ${hitung}\nGrade = E`);
-    break;
-  case hitung >= 60 && hitung <= 69:
-    console.log(`Rata-rata = ${hitung}\nGrade = D`);
-    break;
-  case hitung >= 70 && hitung <= 79:
-    console.log(`Rata-rata = ${hitung}\nGrade = C`);
-    break;
-  // kondisi tidak apa jika tidak dalam scope apapun
-  case hitung >= 80 && hitung <= 89:
-    console.log(`Rata-rata = ${hitung}\nGrade = B`);
-    break;
-  case hitung >= 90 && hitung <= 100:
-    console.log(`Rata-rata = ${hitung}\nGrade = A`);
-    break;
-  default:
-    console.log('anda wajib mengulang');
-    break;
+let test = true;
+
+try {
+  switch (test) {
+    case hitung >= 0 && hitung <= 59:
+      console.log(`Rata-rata = ${hitung}\nGrade = E`);
+      break;
+    case hitung >= 60 && hitung <= 69:
+      console.log(`Rata-rata = ${hitung}\nGrade = D`);
+      break;
+    case hitung >= 70 && hitung <= 79:
+      console.log(`Rata-rata = ${hitung}\nGrade = C`);
+      break;
+    // kondisi tidak apa jika tidak dalam scope apapun
+    case hitung >= 80 && hitung <= 89:
+      console.log(`Rata-rata = ${hitung}\nGrade = B`);
+      break;
+    case hitung >= 90 && hitung <= 100:
+      console.log(`Rata-rata = ${hitung}\nGrade = A`);
+      break;
+    default:
+      console.log('anda wajib mengulang');
+      break;
+  }
+} catch (error) {
+  console.log(`pastikan yang diinputkan adalah angka ${error}`);
 }
